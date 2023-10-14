6a48b31dc55278e67b2e34e31b95789006dff4c1
ImAlagar <143777541+ImAlagar@users.noreply.github.com>
2023-10-14 22:28:50 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:susil/medical.dart';
+
+void main(){
+  runApp(MaterialApp(
+    home: Medicalapp(),
+    debugShowCheckedModeBanner: false,
+
+  ));
+}
+
+
+
+
